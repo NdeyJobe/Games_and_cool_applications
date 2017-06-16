4f2f55f3516f91f5820f0fdb8db8dfecd9fe830e
Ndey Isatou Jobe <isa2jobe@gmail.com>
2017-06-17 00:26:46 +0200

    Update string_concatenation_function.py
    Added the fourth function to the same solution

--- string_concatenation_function.py
+++ string_concatenation_function.py
@@ -25,3 +25,16 @@ def total(numbers):
     return result
 print total (n)
 
+#function 4
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results = []
+    for numbers in lists:
+        for i in numbers:
+            results.append(i)
+    return results
+            
+print flatten(n)
+
+
